14a4d7b0562189126bd71eb4038f66b257f1b3c1
Majoneza <matejbozic1@hotmail.com>
2024-02-12 00:58:25 +0100

    Added service account support for credentials

--- gService/gCredentials.py
+++ gService/gCredentials.py
@@ -6,6 +6,7 @@ from typing import Any, Callable, cast, Literal, Sequence, Optional
 import google.auth
 from google.auth.credentials import Credentials
 from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials as ServiceAccountCredentials
 from google.oauth2.credentials import Credentials as OAuth2Credentials
 from google_auth_oauthlib.flow import Flow, InstalledAppFlow
 
@@ -32,7 +33,7 @@ class gCredentials:
         credentials, project_id = google.auth.default(self._scopes)
         return _gCredentials(self, cast(Any, credentials)), cast(Any, project_id)
 
-    def apiKey(self, key: str) -> gCredentials:
+    def api_key(self, key: str) -> gCredentials:
         return _gCredentialsApiKey(self, key)
 
     def oauth2(
@@ -40,15 +41,52 @@ class gCredentials:
         credentials_path: str = "credentials.json",
         token_path: str = "token.json",
     ) -> gCredentials:
-        return _gCredentialsOauth2(self, credentials_path, token_path)
+        return _gCredentialsRefresh(
+            self,
+            OAuth2Credentials.from_authorized_user_info,
+            credentials_path,
+            token_path,
+        )
 
-    def oauth2url(
+    def oauth2_url(
         self,
         url_callback: Callable[[str], str],
         credentials_path: str = "credentials.json",
         token_path: str = "token.json",
     ) -> gCredentials:
-        return _gCredentialsOauth2url(self, url_callback, credentials_path, token_path)
+        return _gCredentialsRefreshUrl(
+            self,
+            OAuth2Credentials.from_authorized_user_info,
+            url_callback,
+            credentials_path,
+            token_path,
+        )
+
+    def service_account(
+        self,
+        credentials_path: str = "credentials.json",
+        token_path: str = "token.json",
+    ) -> gCredentials:
+        return _gCredentialsRefresh(
+            self,
+            ServiceAccountCredentials.from_service_account_file,
+            credentials_path,
+            token_path,
+        )
+
+    def service_account_url(
+        self,
+        url_callback: Callable[[str], str],
+        credentials_path: str = "credentials.json",
+        token_path: str = "token.json",
+    ) -> gCredentials:
+        return _gCredentialsRefreshUrl(
+            self,
+            ServiceAccountCredentials.from_service_account_file,
+            url_callback,
+            credentials_path,
+            token_path,
+        )
 
 
 class _gCredentials(gCredentials):
@@ -73,16 +111,25 @@ class _gCredentialsApiKey(gCredentials):
         return self._key
 
 
-class _gCredentialsOauth2(gCredentials):
+RefreshCredentials = ServiceAccountCredentials | OAuth2Credentials
+
+
+class _gCredentialsRefresh(gCredentials):
     _credentials_path: str
     _token_path: str
-    _stored_credentials: OAuth2Credentials | None
+    _stored_credentials: RefreshCredentials | None
 
-    def __init__(self, c: gCredentials, credentials_path: str, token_path: str):
+    def __init__(
+        self,
+        c: gCredentials,
+        get_credentials: Callable[[Any, Sequence[str]], RefreshCredentials],
+        credentials_path: str,
+        token_path: str,
+    ):
         super().__init__(c._scopes)
         self._credentials_path = credentials_path
         self._token_path = token_path
-        self._stored_credentials = self._try_load()
+        self._stored_credentials = self._try_load(get_credentials)
 
     def _get_file_path(self, file: Literal["credentials", "token"]):
         match file:
@@ -97,29 +144,29 @@ class _gCredentialsOauth2(gCredentials):
         path = self._get_file_path(file)
         return open(path, mode + "b")
 
-    def _save_credentials_to_file(self, credentials: OAuth2Credentials) -> None:
+    def _save_credentials_to_file(self, credentials: RefreshCredentials) -> None:
         with self._open_file("token", "w") as file:
             file.write(credentials.to_json().encode())
 
     @staticmethod
-    def _update_credentials(credentials: OAuth2Credentials) -> bool:
+    def _update_credentials(credentials: RefreshCredentials) -> bool:
         if credentials.valid:
             return True
-        if credentials.expired and credentials.refresh_token:
+        if credentials.expired:
             credentials.refresh(Request())
         return credentials.valid
 
-    def _try_load(self):
+    def _try_load(
+        self, get_credentials: Callable[[Any, Sequence[str]], RefreshCredentials]
+    ):
         if os.path.exists(self._token_path):
             with self._open_file("token", "r") as file:
-                c = OAuth2Credentials.from_authorized_user_info(
-                    json.load(file), self._scopes
-                )
+                c = get_credentials(json.load(file), self._scopes)
             if self._update_credentials(c):
                 return c
         return None
 
-    def _change_credentials(self, credentails: OAuth2Credentials):
+    def _change_credentials(self, credentails: RefreshCredentials):
         self._save_credentials_to_file(credentails)
         if self._update_credentials(credentails):
             return credentails
@@ -131,7 +178,7 @@ class _gCredentialsOauth2(gCredentials):
             credentials = flow.run_local_server()
         return self._change_credentials(credentials)
 
-    def getStoredCredentials(self) -> Credentials | None:
+    def getStoredCredentials(self) -> RefreshCredentials | None:
         if self._stored_credentials is not None:
             if not self._update_credentials(self._stored_credentials):
                 self._stored_credentials = self._fetch_credentials()
@@ -140,17 +187,18 @@ class _gCredentialsOauth2(gCredentials):
         return self._stored_credentials
 
 
-class _gCredentialsOauth2url(_gCredentialsOauth2):
+class _gCredentialsRefreshUrl(_gCredentialsRefresh):
     _url_callback: Callable[[str], str]
 
     def __init__(
         self,
         c: gCredentials,
+        get_credentials: Callable[[Any, Sequence[str]], RefreshCredentials],
         url_callback: Callable[[str], str],
         credentials_path: str,
         token_path: str,
     ):
-        super().__init__(c, credentials_path, token_path)
+        super().__init__(c, get_credentials, credentials_path, token_path)
         self._url_callback = url_callback
 
     def _fetch_credentials(self):
